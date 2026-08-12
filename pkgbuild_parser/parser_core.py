fb1d9a3e82dd278ef1e237b8054265494d27fc24
KevinCrrl <y62257136@gmail.com>
2026-08-12 12:56:56 -0500

    Add cache option and clear legacy code; migrating to shlex.split

--- pkgbuild_parser/parser_core.py
+++ pkgbuild_parser/parser_core.py
@@ -3,6 +3,7 @@
 #  file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 from platform import machine
+from shlex import split
 
 
 class ParserFileError(Exception):
@@ -24,72 +25,68 @@ def remove_quotes(string) -> list[str] | str:
 
 
 class ParserCore:
-    __slots__ = ('filename', 'lines')
+    __slots__ = ('filename', 'lines', 'enable_cache', 'cache')
 
-    def __init__(self, filename: str = "PKGBUILD"):
+    def __init__(self, filename: str = "PKGBUILD", enable_cache: bool = True):
         try:
             with open(filename, 'r', encoding="utf-8") as f:
-                self.lines = [line.strip() for line in f]
+                self.lines = f.readlines()
+            self.enable_cache = enable_cache
+            if self.enable_cache:
+                self.cache = {}
         except FileNotFoundError as exc:
             raise ParserFileError(f"PKGBUILD file '{filename}' not found") from exc
 
     def multiline(self, key: str) -> list[str]:
-        list_of_lines: list[str] = []
-        key_found: bool = False
-        for line in self.lines:
-            # line example: optdepends=('package: desc' # comment
-            # or
-            # line example: optdepends=('one_package: one_desc') # comment
+        try:
+            return self.cache[key]
+        except (AttributeError, KeyError):
+            list_of_lines: list[str] = []
+            key_found: bool = False
+            for line in self.lines:
+                # line example: optdepends=('package: desc' # comment
+                # or
+                # line example: optdepends=('one_package: one_desc') # comment
 
-            # new line example: optdepends=(one_package: one_desc) or optdepends=(package: desc
-            line: str = remove_quotes(line.split("#")[0].strip())
-            if not key_found and line.startswith(f"{key}="):  # key discovered
-                # fix for depends and makedepends
-                if " " in line and ":" not in line:
-                    list_of_lines = line.split()
-                    list_of_lines[0] = list_of_lines[0].split("=(")[1]
-                else:
-                    list_of_lines.append(line.split("=")[1].lstrip("(").rstrip(
-                        " "))  # new line example: one_package: one_desc) or package: desc
-                key_found = True
-                if not line.startswith(f"{key}=("):  # break when the line is not multiline
+                # new line example: optdepends=(one_package: one_desc) or optdepends=(package: desc
+                line: str = line.split("#")[0].strip()
+                if not key_found and line.startswith(f"{key}="):  # key discovered
+                    # fix for depends and makedepends
+                    if key != "pkgdesc" and " " in line and ":" not in line:
+                        list_of_lines = split(line)
+                        list_of_lines[0] = list_of_lines[0].split("=(")[1]
+                    else:
+                        list_of_lines.append(line.split("=")[1].lstrip("(").rstrip(
+                            " "))  # new line example: one_package: one_desc) or package: desc
+                    key_found = True
+                    if not line.startswith(f"{key}=("):  # break when the line is not multiline
+                        break
+                if key_found and list_of_lines[0].endswith(")"):
+                    # Fix for optdepends arrays
+                    list_of_lines = split(list_of_lines[0].rstrip(")"))
+                    list_of_lines = [package.strip() for package in list_of_lines]  # Quit spaces
                     break
-            if key_found and list_of_lines[0].endswith(")"):
-                # Fix for optdepends arrays
-                if ":" in list_of_lines[0]:
-                    list_of_lines[0] = list_of_lines[0].rstrip(")")
-                else:
-                    list_of_lines = list_of_lines[0].rstrip(")").split()
-                list_of_lines = [package.strip() for package in list_of_lines]  # Quit spaces
-                break
-            if key_found and list_of_lines[-1].endswith(")"):  # Only for depends and makedepends
-                list_of_lines[-1] = list_of_lines[-1].strip(")")
-                break
-            if key_found and not line.endswith(")") and not line.startswith(f"{key}="):
-                if " " in line and ":" not in line:
-                    for package in line.split():
+                if key_found and list_of_lines[-1].endswith(")"):  # Only for depends/makedepends
+                    list_of_lines[-1] = list_of_lines[-1].strip(")")
+                    break
+                if key_found and not line.endswith(")") and not line.startswith(f"{key}="):
+                    for package in split(line):
                         list_of_lines.append(package)
-                else:
-                    list_of_lines.append(line.split("#")[0].strip())
-            if key_found and line.endswith(")"):
-                list_of_lines += line.rstrip(")").strip().split()
-                break
-        list_of_lines = list(filter(None, list_of_lines))
-        if list_of_lines:
-            return self.processvar(list_of_lines)
+                if key_found and line.endswith(")"):
+                    list_of_lines += split(line.rstrip(")").strip())
+                    break
+            list_of_lines = list(filter(None, list_of_lines))
+            if list_of_lines:
+                list_of_lines = [remove_quotes(element) for element in list_of_lines]
+                to_return = self.processvar(list_of_lines)
+                if self.enable_cache:
+                    self.cache[key] = to_return
+                return to_return
         raise ParserKeyError(f"{key} not found in PKGBUILD")
 
     def get_base(self, key: str) -> str:
         """Basic function to obtain simple values."""
-        for line in self.lines:
-            if line.startswith(f"{key}="):
-                # line example: pkgdesc=("desc here") # packager's comment
-                # line.split("=")[1] example: ("desc here") # packager's comment
-                # line.split("=")[1].split("#")[0].lstrip("(").rstrip(")") example: "package info"
-                return self.processvar(remove_quotes(
-                    line.split("=")[1].split("#")[0].lstrip("(").rstrip(")")
-                ))
-        raise ParserKeyError(f"{key} not found in PKGBUILD")
+        return self.multiline(key)[0]
 
     def replacevar(self, var: str) -> str:
         names: list[str] = []
